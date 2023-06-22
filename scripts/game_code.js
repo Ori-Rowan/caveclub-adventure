2151c9b998eaf947702c0372e20773643d48d209
Ori <ori.rowan@gmail.com>
2023-06-22 09:30:16 +0200

    scene background
    function that sets background for scene

---
+++ scripts/game_code.js
@@ -0,0 +1,19 @@
+/** Set background img
+* * Sets the background for a scene.
+* ? Property background-img to the #gameWindow <div>
+* @param img - file name, in 'scene/' folder
+*/
+
+function set_background(img) {
+    var path = 'src/img/scene/'+img;
+    $('#gameWindow').css(
+        'background-image',
+        'url("'+path+'")'
+    );
+}
+
+
+// Start the code
+$(document).ready(function(){
+    set_background('test-1.jpg');
+})
