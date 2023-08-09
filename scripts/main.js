b671938738694ae6f8b9a9f5130410deeeeb4547
Orion Rowan <ori.rowan@gmail.com>
2023-08-10 00:40:01 +0200

    test-ready
    changing the src images so they are more ready for testing. Also adding a gameScript object with 2 scenes ready

--- scripts/main.js
+++ scripts/main.js
@@ -2,22 +2,44 @@ $(document).ready(function () {
 	game = new Game();
 
 	//test scene
-	scene = {
-		background: "test-1.jpg",
-		sprites: {
-			sprite1: {
-				img: "test-sprite1.png",
-				coords: { x: 50, y: 100 },
-				properties: {},
+	gameScript = {
+		scene1: {
+			background: "test_background_1.jpg",
+			sprites: {
+				sprite1: {
+					img: "test_sprite_1.png",
+					coords: { x: 50, y: 100 },
+					properties: {},
+				},
+				sprite2: {
+					img: "test_sprite_2.png",
+					coords: { x: 200, y: 200 },
+					properties: {},
+				},
+				sprite3: {
+					img: "test_sprite_4.png",
+					coords: { x: 500, y: 200 },
+					properties: {},
+				},
 			},
-			sprite2: {
-				img: "test-sprite2.png",
-				coords: { x: 200, y: 200 },
-				properties: {},
+		},
+        scene2: {
+			background: "test_background_2.jpg",
+			sprites: {
+				sprite1: {
+					img: "test_sprite_3.png",
+					coords: { x: 50, y: 600 },
+					properties: {},
+				},
+				sprite2: {
+					img: "test_sprite_4.png",
+					coords: { x: 800, y: 200 },
+					properties: {},
+				},
 			},
 		},
 	};
 
 	//test
-	game.loadScene(scene);
+	game.loadScene(gameScript.scene1);
 });
